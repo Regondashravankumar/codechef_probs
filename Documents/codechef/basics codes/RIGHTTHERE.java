d4e9de76e031c447455a29b1ba46d9721f52429a
Regondashravankumar <137642528+Regondashravankumar@users.noreply.github.com>
2023-06-27 23:32:44 +0530

    Add files via upload

---
+++ Documents/codechef/basics codes/RIGHTTHERE.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int t=sc.nextInt();
+	    for(int i=0; i<t; i++)
+	    {
+	        int a=sc.nextInt();
+	        int b=sc.nextInt();
+	        if(a<b||a==b)
+	         System.out.println("YES");
+	        else
+	         System.out.println("NO");
+	        
+	    }
+	}
+		
+}
+
---
+++ Documents/codechef/basics codes/TASTEDEC.java
@@ -0,0 +1,37 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class  Main
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		   int a=sc.nextInt();
+		   int b=sc.nextInt();
+		  
+		   
+		   
+		   if(2*a>5*b)
+		   {
+		       System.out.println("Chocolate");
+		       
+		   }
+		   else if(2*a==5*b){
+		       System.out.println("Either");
+		       
+		   }
+		   else{
+		       System.out.println("Candy");
+		   }
+		}
+		  
+	}
+	
+}
