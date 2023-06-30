c4ff1841f5f9fba254da89994e60fdb86c126507
Regondashravankumar <137642528+Regondashravankumar@users.noreply.github.com>
2023-06-30 13:02:21 +0530

    Add files via upload

---
+++ Documents/codechef/basics codes/SPECIALITY.java
@@ -0,0 +1,40 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0; i<t; i++)
+		{  
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    if(a>b&&b>c)
+		    {
+		        System.out.println("Setter");
+		    }
+		     else if(a<b&&b<c)
+		    {
+		        System.out.println("Editorialist");
+		    }
+		    else if(a<b&&b>c)
+		    {
+		        System.out.println("Tester");
+		    }
+		    else{
+		           System.out.println("Setter");
+		        }
+		    //{
+		        //System.out.println("Setter");
+		   // }
+		 }
+	}
+}
+
